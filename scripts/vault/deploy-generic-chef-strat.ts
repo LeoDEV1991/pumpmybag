29c9c549a7c77e9f675f7101706e0ed4ec0120a8
AlexandrGergel <94127898+LeoDEV1991@users.noreply.github.com>
2022-04-05 11:00:33 +0800

    initial

---
+++ scripts/vault/deploy-generic-chef-strat.ts
@@ -0,0 +1,133 @@
+import hardhat, { ethers, web3 } from "hardhat";
+import { addressBook } from "blockchain-addressbook";
+import { predictAddresses } from "../../utils/predictAddresses";
+// import { setCorrectCallFee } from "../../utils/setCorrectCallFee";
+// import { setPendingRewardsFunctionName } from "../../utils/setPendingRewardsFunctionName";
+import { verifyContract } from "../../utils/verifyContract";
+import { BeefyChain } from "../../utils/beefyChain";
+
+const registerSubsidy = require("../../utils/registerSubsidy");
+
+const {
+  platforms: { mdex, beefyfinance },
+  tokens: {
+    MDX: { address: MDX },
+    BUSD: { address: BUSD },
+    LAC: { address: LAC },
+    WBNB: { address: WBNB },
+  },
+} = addressBook.bsc;
+
+const shouldVerifyOnEtherscan = false;
+
+const want = web3.utils.toChecksumAddress("0xa269E050DD2262E7BdC8A481D76880A562dD1d5E");
+
+const vaultParams = {
+  mooName: "Moo Mdex LAC-BUSD",
+  mooSymbol: "mooMdexLAC-BUSD",
+  delay: 21600,
+};
+
+const strategyParams = {
+  want,
+  poolId: 136,
+  chef: mdex.masterchef,
+  unirouter: mdex.router,
+  strategist: "0x6755b6F2067C65ca17C908789834FCdA2714A455", // some address
+  keeper: beefyfinance.keeper,
+  beefyFeeRecipient: beefyfinance.beefyFeeRecipient,
+  outputToNativeRoute: [MDX, WBNB],
+  outputToLp0Route: [MDX, BUSD, LAC],
+  outputToLp1Route: [MDX, BUSD],
+//  pendingRewardsFunctionName: "pendingCake", // used for rewardsAvailable(), use correct function name from masterchef
+};
+
+const contractNames = {
+  vault: "BeefyVaultV6",
+  // strategy: "StrategyMdexChefLP",
+  strategy: "StrategyCommonChefLP",
+};
+
+async function main() {
+  if (
+    Object.values(vaultParams).some(v => v === undefined) ||
+    Object.values(strategyParams).some(v => v === undefined) ||
+    Object.values(contractNames).some(v => v === undefined)
+  ) {
+    console.error("one of config values undefined");
+    return;
+  }
+
+  await hardhat.run("compile");
+
+  const Vault = await ethers.getContractFactory(contractNames.vault);
+  const Strategy = await ethers.getContractFactory(contractNames.strategy);
+
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying:", vaultParams.mooName);
+
+  const predictedAddresses = await predictAddresses({ creator: deployer.address });
+
+  const vaultConstructorArguments = [
+    predictedAddresses.strategy,
+    vaultParams.mooName,
+    vaultParams.mooSymbol,
+    vaultParams.delay,
+  ];
+  const vault = await Vault.deploy(...vaultConstructorArguments);
+  await vault.deployed();
+
+  const strategyConstructorArguments = [
+    strategyParams.want,
+    strategyParams.poolId,
+    strategyParams.chef,
+    vault.address,
+    strategyParams.unirouter,
+    strategyParams.keeper,
+    strategyParams.strategist,
+    strategyParams.beefyFeeRecipient,
+    strategyParams.outputToNativeRoute,
+    strategyParams.outputToLp0Route,
+    strategyParams.outputToLp1Route,
+  ];
+  const strategy = await Strategy.deploy(...strategyConstructorArguments);
+  await strategy.deployed();
+
+  // add this info to PR
+  console.log();
+  console.log("Vault:", vault.address);
+  console.log("Strategy:", strategy.address);
+  console.log("Want:", strategyParams.want);
+  console.log("PoolId:", strategyParams.poolId);
+
+  console.log();
+  console.log("Running post deployment");
+
+  const verifyContractsPromises: Promise<any>[] = [];
+  if (shouldVerifyOnEtherscan) {
+    // skip await as this is a long running operation, and you can do other stuff to prepare vault while this finishes
+    verifyContractsPromises.push(
+      verifyContract(vault.address, vaultConstructorArguments),
+      verifyContract(strategy.address, strategyConstructorArguments)
+    );
+  }
+ // await setPendingRewardsFunctionName(strategy, strategyParams.pendingRewardsFunctionName);
+ // await setCorrectCallFee(strategy, hardhat.network.name as BeefyChain);
+  console.log();
+
+  await Promise.all(verifyContractsPromises);
+
+  if (hardhat.network.name === "bsc") {
+    await registerSubsidy(vault.address, deployer);
+    await registerSubsidy(strategy.address, deployer);
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
+  
